7818c5471360587ed0f36c1940aad63c31f99420
GustaStarz <gustavohackaton@gmail.com>
2026-04-15 16:45:30 -0300

    patch6

--- Scripts/neb.gd
+++ Scripts/neb.gd
@@ -2,6 +2,18 @@ extends CharacterBody2D
 
 const SPEED = 300
 var direction = Vector2(0,0)
+
+@export var projetil : PackedScene
+@onready var ponta = $EmissorBala
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("test"):
+		disparar()
+		
+func disparar():
+	var nova_bala = projetil.instantiate()
+	nova_bala.global_position = ponta.global_position
+	get_tree().current_scene.add_child(nova_bala)
+	
 func _physics_process(delta: float) -> void:
 	mover()
 	move_and_slide()
